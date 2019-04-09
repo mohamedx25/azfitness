f6c46c8f65bcaf0236714a836f13c800fafce083
Mohamed Farih <othmanx23@gmail.com>
2019-04-09 16:31:10 -0400

    Finish home page
    Finish the basic structure of home page will need to add animations & functionality

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,59 @@
 import React from 'react'
 import './Footer.css'
-import { Link } from "gatsby"
+
 import './images/insta.svg'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab);
 
 class Footer extends React.Component{
     render(){
         return(
             <div className='footer'>
                 <div className='social'>
-                    <li><Link to='/' className='fa fa-instagram'></Link></li>
-                    <li><Link to='/'><FontAwesomeIcon icon="igloo" /></Link></li>
-                    <li><Link to='/'></Link></li>
+                    <ul>
+                        <li><a href='/'>
+                            <FontAwesomeIcon className='insta' icon={['fab', 'instagram']} size='2x'
+                                style={{
+                                    color: '#fff'
+                                }}
+                            />
+                        </a></li>
+                        <li><a href='/'>
+                            <FontAwesomeIcon className='twitter' icon={['fab', 'twitter']} size='2x'
+                                style={{
+                                    color: '#fff'
+                                }}
+                            />
+                        </a></li>
+                        <li><a href='/'>
+                            <FontAwesomeIcon className='youtube' icon={['fab', 'youtube']} size='2x'
+                                style={{
+                                    color: '#fff'
+                                }}
+                                
+                            />    
+                        </a></li>
+                        <li><a href='/'>
+                            <FontAwesomeIcon className='linkedin' icon={['fab', 'linkedin']} size='2x'
+                                activeClassName='active'
+                                style={{
+                                    color: '#fff'
+                                }}
+                            />    
+                        </a></li>
+                    </ul>
+                </div>
+                <div className='company'>
+                    <h5>A2Z Fitness LLC {'\u00A9'}</h5>
                 </div>
              </div>
         )
     }
 }
-
- 
-
 
 export default Footer;

f08f9c4fd2373b263e8268905161c0f79efe6dcd
Mohamed Farih <othmanx23@gmail.com>
2019-04-15 14:46:35 -0400

    added skeleton for contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -31,57 +31,56 @@ const Contact = (props) =>(
         <div className='contact'>
             <h1>Contact Me</h1>
         </div>
-        <div>
-        <section className='form-section'>
-       
-            <div className='contact-form'>
-                <form method='post' action=''>
-                    <div>
-                        <label>First Name</label>
-                        <input type="text" name="name" id="f_name" />
-                    </div>
-                    <div>
-                        <label>Last Name</label>
-                        <input type="text" name="name" id="l_name" />
-                    </div>
-                    <div>
-                        <label>Cell Phone</label>
-                        <input type="text" name="phone" id="cell" />
-                    </div>
-                    <div>
-                        <label>E-mail</label>
-                        <input type="text" name="email" id="email" />
-                    </div>
-                    <div>
-                        <label>Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
-                    </div>
-                    <div>
-                        <input type="submit" value="Submit" style={buttonStyles}/>
-                    </div>
-                </form>
-            </div>
-        </section>
-        <section className='info-section'>
-            <div className='email-info'>
-                <FontAwesomeIcon icon={faEnvelope} size='3x'/>
-                <h3>Email</h3>
-                <button>a2zfitness@gmail.com</button>
-            </div>
-            <div className='phone-info'>
-                <FontAwesomeIcon icon={faPhone} size='3x'/>
-                <h3>Phone Number</h3>
-                <span>123-939-2020</span>
-            </div>
-            <div className='address-info'>
-                <FontAwesomeIcon icon={faHome} size='3x'/>
-                <h3>Gym Location</h3>
-                <span>1234 Somewhere Road #5432<br />
-                    Kearny, NJ 00000<br />
-                    United States of America</span>
-            </div>
-            
-        </section>
+        <div className='info-container'>
+            <section className='form-section'>
+                <div className='contact-form'>
+                    <form method='post' action=''>
+                        <div>
+                            <label>First Name</label>
+                            <input type="text" name="name" id="f_name" />
+                        </div>
+                        <div>
+                            <label>Last Name</label>
+                            <input type="text" name="name" id="l_name" />
+                        </div>
+                        <div>
+                            <label>Cell Phone</label>
+                            <input type="text" name="phone" id="cell" />
+                        </div>
+                        <div>
+                            <label>E-mail</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div>
+                            <label>Message</label>
+                            <textarea name="message" id="message" rows="6"></textarea>
+                        </div>
+                        <div>
+                            <input type="submit" value="Submit" style={buttonStyles}/>
+                        </div>
+                    </form>
+                </div>
+            </section>
+            <section className='info-section'>
+                <div className='email-info'>
+                    <FontAwesomeIcon icon={faEnvelope} size='3x'/>
+                    <h3>Email</h3>
+                    <button>a2zfitness@gmail.com</button>
+                </div>
+                <div className='phone-info'>
+                    <FontAwesomeIcon icon={faPhone} size='3x'/>
+                    <h3>Phone Number</h3>
+                    <span>123-939-2020</span>
+                </div>
+                <div className='address-info'>
+                    <FontAwesomeIcon icon={faHome} size='3x'/>
+                    <h3>Gym Location</h3>
+                    <span>1234 Somewhere Road #5432<br />
+                        Kearny, NJ 00000<br />
+                        United States of America</span>
+                </div>
+                
+            </section>
         </div>
         <Footer />
     </div>

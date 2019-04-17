7312c3f7d4f0cce3c9ef20152b4287e47fcd53d7
Mohamed Farih <othmanx23@gmail.com>
2019-04-17 13:43:01 -0400

    commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,8 @@
 import React  from 'react';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import { Form } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import '../../components/Header.css'
 import '../../components/Footer.css'
 import '../../components/styles/Contact.css'
@@ -13,17 +15,17 @@ import {faHome} from '@fortawesome/free-solid-svg-icons'
 
 library.add(fab);
 
-const buttonStyles = {
-    fontSize: "13px",
-    textAlign: "center",
-    color: "#fff",
-    outline: "none",
-    padding: "12px 60px",
-    boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
-    backgroundColor: "#5ff442",
-    borderRadius: "6px",
-    letterSpacing: "1.5px",
-}
+// const buttonStyles = {
+//     fontSize: "13px",
+//     textAlign: "center",
+//     color: "#fff",
+//     outline: "none",
+//     padding: "12px 60px",
+//     boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
+//     backgroundColor: "#5ff442",
+//     borderRadius: "6px",
+//     letterSpacing: "1.5px",
+// }
 
 const Contact = (props) =>(
     <div>
@@ -34,17 +36,61 @@ const Contact = (props) =>(
         <div className='info-container'>
             <section className='form-section'>
                 <div className='contact-form'>
-                    <form method='post' action=''>
+                    <Form>
+                        <Form.Group controlId='formBasicName'>
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control 
+                                type='text' 
+                                placeholder='Enter First Name'/>
+                        </Form.Group>
+                        <Form.Group controlId='formBasicName'>
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control 
+                                type='text' 
+                                placeholder='Enter Last Name'/>
+                        </Form.Group>
+                        <Form.Group controlId='formGroupEmail'>
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control 
+                                type='text' 
+                                placeholder='Enter Email'/>
+                        </Form.Group>
+                        <Form.Group controlId='formGroupNumber'>
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control 
+                                type='number' 
+                                placeholder='Enter Phone number'/>
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control 
+                                as="textarea" 
+                                rows="6" 
+                                placeholder='Send me any questions or comments'
+                            />
+                        </Form.Group>
+                        <Button variant='success' 
+                                size='lg' 
+                                active
+                                >
+                            Submit
+                        </Button>
+                    </Form>
+                    {/* <form method='post' action=''>
                         <div>
                             <label>First Name
                             <input 
-                                type="text" name="Fname" id="f_name" />
+                                type="text" 
+                                name="Fname" 
+                                id="f_name" />
                             </label>
                         </div>
                         <div>
                             <label>Last Name
                             <input 
-                                type="text" name="Lname" id="l_name" />
+                                type="text" 
+                                name="Lname" 
+                                id="l_name" />
                             </label>
                         </div>
                         <div>
@@ -74,23 +120,32 @@ const Contact = (props) =>(
                         </div>
                         <div>
                             <input type="submit" value="Submit" style={buttonStyles}/>
-                        </div>
-                    </form>
+                        </div> 
+                    </form>*/} 
                 </div>
             </section>
             <section className='info-section'>
                 <div className='email-info'>
-                    <FontAwesomeIcon icon={faEnvelope} size='3x'/>
+                    <FontAwesomeIcon icon={faEnvelope} 
+                        size='3x'
+                        className='email-icon'
+                    />
                     <h3>Email</h3>
                     <button>a2zfitness@gmail.com</button>
                 </div>
                 <div className='phone-info'>
-                    <FontAwesomeIcon icon={faPhone} size='3x'/>
+                    <FontAwesomeIcon 
+                        icon={faPhone} 
+                        size='3x'
+                        className='phone-icon'/>
                     <h3>Phone Number</h3>
                     <span>123-939-2020</span>
                 </div>
                 <div className='address-info'>
-                    <FontAwesomeIcon icon={faHome} size='3x'/>
+                    <FontAwesomeIcon 
+                        icon={faHome} 
+                        size='3x'
+                        className='home-icon'/>
                     <h3>Gym Location</h3>
                     <span>1234 Somewhere Road #5432<br />
                         Kearny, NJ 00000<br />

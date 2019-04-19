3b78ef69edde8f5be3fe640de8dcb517ee55b6e9
Mohamed Farih <othmanx23@gmail.com>
2019-04-19 15:56:27 -0400

    commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './Footer.css'
-
-import './images/insta.svg'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'

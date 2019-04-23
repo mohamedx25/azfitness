537bab2997694db4efb900475a6d240edf8f018d
Mohamed Farih <othmanx23@gmail.com>
2019-04-23 14:04:12 -0400

    commit
    Testing why the success is not working

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,8 @@
 import React  from 'react';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import { Form } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
+// import { Form } from 'react-bootstrap'
+// import { Button } from 'react-bootstrap'
 import '../../components/Header.css'
 import '../../components/Footer.css'
 import '../../components/styles/Contact.css'
@@ -36,8 +36,8 @@ const Contact = (props) =>(
         <div className='info-container'>
             <section className='form-section'>
                 <div className='contact-form'>
-                    <Form name='contact' 
-                        action='/about/'
+                    {/* <Form name='contact' 
+                        action='/success/'
                         method='POST' 
                         data-netlify='true'
                         data-netlify-honeypot='bot-field'>
@@ -85,17 +85,18 @@ const Contact = (props) =>(
                                 >
                             Submit
                         </Button>
-                    </Form>
-                    {/* <form 
+                    </Form> */}
+                    <form 
                         method='POST'
                         name='contact'
+                        action='/success/'
                         data-netlify='true'
                         data-netlify-honeypot='bot-field'>
                         Full Name <input type='text'
                             name='name'
                             placeholder='your name'/>
                         <button type='submit'>Submit</button>
-                    </form> */}
+                    </form>
                 </div>
             </section>
             <section className='info-section'>

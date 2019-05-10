c77e61a41974faf67eb2e83833fc5c3534426c84
Mohamed Farih <othmanx23@gmail.com>
2019-05-10 17:04:10 -0400

    commit
    Added the react bootstrap slideshow. need to fix the sizing of the slideshow

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import Video from '../../components/Video'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import SlideShow from '../../components/SlideShow'
+import { Carousel } from 'react-bootstrap'
+// import SlideShow from '../../components/SlideShow'
 import '../../components/styles/App.css'
 import group from '../../components/images/promo.mp4'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css' 
-
-
+import soccer from '../../components/images/soccer.jpg'
+import boxing from '../../components/images/boxing.jpg'
+import beach from '../../components/images/beach.jpg'
 
 class App extends React.Component{
     // testing dep
@@ -18,7 +20,7 @@ class App extends React.Component{
                 <Header />
                 <Video />
                  {/*personal training section*/}
-                <div className='pt-training'>
+                {/* <div className='pt-training'>
                     <div className='pt-about'>
                         <div className='pt-info'>
                             <h1>Personal Training</h1>
@@ -33,26 +35,19 @@ class App extends React.Component{
                             </div>
                         </div>
                     </div>
-                </div>
-                
-
-                {/*Sports training section*/}
-                {/* <div className='skills-training'>
-                    <div className='skills'>
-                        <div className='slide slide1'>
-                            <div className='skills-content'>
-                                <h1>Skills Training section</h1>
-                                <p>My sports skills training is elite. I guarantee that you will consistently
-                                see continued growth and development.  My program will make you become the strongest
-                                and fastest you have ever been.  As well as keep you in the best shape</p>
-                                <div className='skills-button'>
-                                    
-                                    <button className='skills-btn'>Join Now</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div> */}
+                
+                <div className='container-fluid img-fluid'>
+                    <div className='col-lg pt-info'>
+                        <h3>Personal Training</h3>
+                        <p>My personal training program is the best around.  I do not guarantee
+                        quick unrealistic results.  I guarantee a system that will allow you to 
+                        reach your fitness goals while still being able to enjoy the foods you like.
+                        </p>
+                        <button type='button' className='btn btn-primary btn-lg'>Learn More</button>
+                    </div>
+                </div>
+
                 {/* fitness quote */}
                 <div className='fitness-quote'>
                     <div className='left-side'></div>
@@ -64,11 +59,68 @@ class App extends React.Component{
                     <div className='right-side'></div>
                 </div>
 
+                {/*Sports training section*/}
+                {/* <div className='skills-training'>
+                    <div className='skills'>
+                        <div className='slide slide1'>
+                            <div className='skills-content'>
+                                <h1>Skills Training section</h1>
+                                <p>My sports skills training is elite. I guarantee that you will consistently
+                                see continued growth and development.  My program will make you become the strongest
+                                and fastest you have ever been.  As well as keep you in the best shape</p>
+                                <div className='skills-button'>
+                                    <button className='skills-btn'>Join Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+                <div className='sports-training'>
+                <Carousel>
+                    <Carousel.Item>
+                        <img 
+                            className='d-block w-100'
+                            src={soccer}
+                            alt='First slide'
+                            
+                        />
+                        <Carousel.Caption>
+                            <h3>Sports Training</h3>
+                            <p>I offer great sports training</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img 
+                            className='d-block w-100'
+                            src={boxing}
+                            alt='second slide'
+                            
+                        />
+                        <Carousel.Caption>
+                            <h3>Sports Training</h3>
+                            <p>I offer great sports training</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img 
+                            className='d-block w-100'
+                            src={beach}
+                            alt='Third slide'
+                            
+                        />
+                        <Carousel.Caption>
+                            <h3>Sports Training</h3>
+                            <p>I offer great sports training</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                </div>
+
                 {/*I will import my slideshow component after the personal training section*/}
-                <SlideShow />
+                {/* <SlideShow /> */}
 
                 {/* Group fitness section */}
-                {/* <div className='group-training'>
+                <div className='group-training'>
                     <div className='group-info'>
                         <h1>Group fitness training</h1>
                         <p>I also lead Group fitness classes at Signature Fitness, Belleville NJ and 
@@ -81,7 +133,7 @@ class App extends React.Component{
                         <video src={group} type='video/mp4' controls='controls' width='750' height='500'/>
                     </div>
                    
-                </div>  */}
+                </div> 
                 <Footer />
             </div>
         );

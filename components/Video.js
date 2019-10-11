beabc7f764b990b58c023d1085512df0c3fd324a
Mohamed Farih <othmanx23@gmail.com>
2019-10-11 11:11:09 -0400

    commit

--- components/Video.js
+++ components/Video.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Video.css'
-import promo from './images/HIIT-video.mp4'
+import promo from './images/promo.mp4'
 
 const Video = () =>(
     <div>
-    <div className='video'>
+    <div className='video container'>
         <div className='fullscreen-video-wrap'>
             <video className='background-video' loop autoPlay muted>
                 <source src={promo} type='video/mp4'/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,24 +19,6 @@ class App extends React.Component{
             <div>
                 <Header />
                 <Video />
-                 {/*personal training section*/}
-                {/* <div className='pt-training'>
-                    <div className='pt-about'>
-                        <div className='pt-info'>
-                            <h1>Personal Training</h1>
-                            <p>My personal training is top of the line.  I do not 
-                                guarantee quick easy results. I guarantee a plan that will 
-                                give you the ability to have sustained success for the future.
-                                I guarantee you will love the way you look and enjoy your fitness journey
-                                training how you want and enjoying all the food you like.
-                            </p>
-                            <div className='pt-button'>
-                                <button className='pt-btn'>Learn More</button>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-                
                 <div className='container-fluid img-fluid'>
                     <div className='col-lg pt-info'>
                         <h3>Personal Training</h3>
